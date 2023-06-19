fae292cdeda2a195d722317019f24a739952d6a8
Sheila Seruya <Sheilaseruya@gmail.com>
2023-06-19 22:18:50 +0300

    done

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -5,4 +5,8 @@ class StudentsController < ApplicationController
     render json: students
   end
 
+  def show
+    students = Student.find_by(id: params[:id])
+    render json: students
+  end
 end
